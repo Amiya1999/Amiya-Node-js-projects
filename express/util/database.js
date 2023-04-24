2af2aba6970bb37cfb8b1e2e955e227d701a60d6
Amiya1999 <45040373+Amiya1999@users.noreply.github.com>
2023-04-24 16:03:48 +0530

    Delete express directory

--- express/util/database.js
+++
@@ -1,9 +0,0 @@
-
-
-const Sequelize=require('sequelize');
-
-const sequelize=new Sequelize('node-complete','root','Shawack@1999',{
-    dialect:'mysql',
-    host:'localhost'
-});
-module.exports=sequelize;
